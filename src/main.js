c2dda9ab15bc9a2d1d694cb744deca4ad540e763
umfrancisco <francis.guitler@gmail.com>
2025-08-11 09:47:06 -0300

    added sections _available-devices and _faq

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(button) {
@@ -12,7 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
             
         })
     }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', toggleAnswerBar)
+    }
 })
+
+function toggleAnswerBar(elemento) {
+    const clazz = 'faq__questions__item--is-open';
+    console.log(elemento);
+    const parentElement = elemento.target.parentNode;
+
+    parentElement.classList.toggle(clazz);
+}
 
 function removeActiveButton() {
     const buttons = document.querySelectorAll('[data-tab-button]');
